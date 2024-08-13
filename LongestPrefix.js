107a9b3e662b6fcbded641136b547ed4c8bb8d3d
Anand . <anandsmathpati18@gmail.com>
2024-08-13 15:24:13 +0530

    [Anand] compltetd Longest prefix Problem

---
+++ LongestPrefix.js
@@ -0,0 +1,24 @@
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+}
+
+
+const input = ["intelliBlue", "intelliFeeder", "intelliAuth"];
+const result = longestCommonPrefix(input);
+console.log("Longest common prefix =", result); 
+
+
+const input1 = ["flower", "flow", "flight"];
+const result1 = longestCommonPrefix(input1);
+console.log("Longest common prefix =", result1);
